ef98e05da0b7890d29f852deeb1685bc76311cbd
Prasanna Tuladhar <tuladharprasan@gmail.com>
2022-09-13 14:47:19 +0200

    Google analytics updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,28 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      
+      <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-JEYE1RNNJV"/>
+<Script strategy="lazyOnload">
+  {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JEYE1RNNJV', {
+    page_path: window.location.pathname,
+    });
+  `}
+</Script>
+<Head />
       <body 
     //   className="bg-white"
       >
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WGQ35MD"
-height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+        {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WGQ35MD"
+height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript> */}
         <Main />
         <NextScript />
       </body>
